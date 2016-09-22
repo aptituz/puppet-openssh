055c8e57d312a754d099a9b27d313c300d878791
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2016-09-22 13:32:00 +0200

    Some polishing to ssh_sign_certificate
    Add puppet-strings based function documentation with an example.
    Some ordering and cruft-removing.

--- lib/puppet/parser/functions/ssh_sign_certificate.rb
+++ lib/puppet/parser/functions/ssh_sign_certificate.rb
@@ -2,7 +2,36 @@
 require 'fileutils'
 
 module Puppet::Parser::Functions
-  newfunction(:ssh_sign_certificate, :type => :rvalue) do |args|
+  newfunction(:ssh_sign_certificate, :type => :rvalue, :doc <<-DOC
+    This function allows generation and puppetmaster-side caching of SSH certificates,
+    for user certificates (default) and host certificates.
+
+    Beneath the required parameters (see below) it can take a hash with some options that
+    refer to ssh-keygen arguments. The following shows the supported parameters
+    and a brief description. For further information see the ssh-keygen manpage.
+
+    - validity: How long should the certificate be valid (-V)
+    - serial_number: What to use as serial number (-z)
+    - host_certifcate: Boolean to indicate that you want to generate a host certificate (-h)
+    - principals: Array of principals set on the key (-n)
+    - cert_options: Array of options to set on the certifiate (e.g. no-tty and alike, refers to -O)
+
+    @param [String] signkey_file    The path (on the puppet master) to a ssh private key used as CA.
+    @param [String] certificate_id  An id for the ssh certificate to generate (-I argument to ssh-keygen)
+    @param [String] public_key      The public key which shall be signed by the CA.
+    @param [Hash]   additional options
+    @return [String] the signed certificate
+    @example Example usage:
+      $public_key  = $::sshrsakey
+      $certificate = ssh_sign_certificate('/etc/puppet/id_ca', $::fqdn, $public_key, { host_certificate: true } )
+
+      file { '/etc/ssh/ssh_host_rsa_key-cert.pub':
+        ensure  => present,
+        content => $certificate
+      }
+
+    DOC
+    ) do |args|
     raise ArgumentError, ("ssh_sign_certificate(): wrong number of arguments (#{args.length}; must be >= 2)") if args.length < 2
 
     signkey_file    = args[0]
@@ -12,9 +41,9 @@ module Puppet::Parser::Functions
     valid_options   = {
         'validity'           => ['-V', '%s'],
         'serial_number'      => ['-z', '%s'],
-        'cert_options'       => proc { |options| options.collect { |opt| ['-O', opt] } },
         'host_certificate'   => '-h',
         'principals'         => proc { |options| [ "-n",  options.join(",") ] },
+        'cert_options'       => proc { |options| options.collect { |opt| ['-O', opt] } },
     }
 
     keygen_extra_args = []
@@ -46,9 +75,7 @@ module Puppet::Parser::Functions
         raise ArgumentError, ("ssh_sign_certificate(): third argument needs to be a string containing public key")
     end
 
-    confdir     = Puppet[:confdir]
     vardir      = Puppet[:vardir]
-
     # used for caching generated certificates
     cache_dir   = File.join(vardir, 'ssh', 'certificates')
     FileUtils.mkdir_p(cache_dir)
