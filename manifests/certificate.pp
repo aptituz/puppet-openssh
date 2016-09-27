9e8f73ae7155f366ab191176519074d1cbdfbc58
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2016-09-27 11:45:30 +0200

    ssh::certificate: some bugfixes, more checks

--- manifests/certificate.pp
+++ manifests/certificate.pp
@@ -2,13 +2,16 @@ define ssh::certificate (
     $ensure = 'present',
     $certificate_id,
     $public_key,
-    $target,
+    $target = $name,
     $host_certificate = false,
     $ca_key_file = '/etc/puppet/id_ca',
     $options = {}
 ) {
     validate_string($certicate_id)
     validate_bool($host_certificate)
+    validate_string($ca_key_file)
+    validate_string($public_key)
+    validate_hash($options)
 
     $real_options = merge($options, {
         'host_certificate' => $host_certificate,
@@ -16,7 +19,7 @@ define ssh::certificate (
 
     file { $target:
         ensure  => $ensure,
-        content => ssh_sign_certificate($ca_key_file, $certicate_id, $public_key, $real_options),
+        content => ssh_sign_certificate($ca_key_file, $certificate_id, $public_key, $real_options),
         mode    => '0644',
     }
 }
